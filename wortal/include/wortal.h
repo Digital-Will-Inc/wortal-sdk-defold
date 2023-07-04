617fd72a9c1a50ca56305f8a46d4850cc8891290
Tim Dillon <tim@itsdits.com>
2023-07-04 17:54:07 +0900

    feat(core): add haptic feedback api

--- wortal/include/wortal.h
+++ wortal/include/wortal.h
@@ -7,15 +7,19 @@
 class Wortal {
 public:
     typedef void (*OnPauseCallback)(const int success);
+    typedef void (*OnHapticFeedbackCallback)(const int success, const char* error);
 
     static int SetPauseCallback(lua_State* L);
+    static int PerformHapticFeedback(lua_State* L);
 
 private:
     static void OnPause(const int success);
+    static void OnHapticFeedback(const int success, const char* error);
 };
 
 extern "C" {
     void Wortal_onPause(Wortal::OnPauseCallback callback);
+    void Wortal_performHapticFeedback(Wortal::OnHapticFeedbackCallback callback);
 }
 
 #endif
