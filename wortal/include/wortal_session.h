854bad7d97f20d6fb6fe844cec6b81f748e07e27
Tim Dillon <tim@itsdits.com>
2023-08-25 09:24:47 +0900

    feat(session): add new apis
    * getDevice, getOrientation, onOrientationChange, switchGameAsync, ads.isAdBlocked

--- wortal/include/wortal_session.h
+++ wortal/include/wortal_session.h
@@ -7,6 +7,8 @@
 class WortalSession {
 public:
     typedef void (*OnGetEntryPointCallback)(const char* data, const char* error);
+    typedef void (*OnOrientationChangeCallback)(const char* orientation);
+    typedef void (*OnSwitchGameCallback)(const int success, const char* error);
 
     static int GetEntryPointData(lua_State* L);
     static int GetLocale(lua_State* L);
@@ -14,9 +16,15 @@ public:
     static int SetSessionData(lua_State* L);
     static int GetEntryPointAsync(lua_State* L);
     static int GetPlatform(lua_State* L);
+    static int GetDevice(lua_State* L);
+    static int GetOrientation(lua_State* L);
+    static int OnOrientationChange(lua_State* L);
+    static int SwitchGameAsync(lua_State* L);
 
 private:
     static void OnGetEntryPoint(const char* entryPoint, const char* error);
+    static void OnOrientationChange(const char* orientation);
+    static void OnSwitchGame(const int success, const char* error);
 };
 
 extern "C" {
@@ -26,6 +34,10 @@ extern "C" {
     void Wortal_session_setSessionData(const char* data);
     void Wortal_session_getEntryPointAsync(WortalSession::OnGetEntryPointCallback callback);
     char* Wortal_session_getPlatform();
+    char* Wortal_session_getDevice();
+    char* Wortal_session_getOrientation();
+    void Wortal_session_onOrientationChange(WortalSession::OnOrientationChangeCallback callback);
+    void Wortal_session_switchGameAsync(const char* gameID, WortalSession::OnSwitchGameCallback callback);
 }
 
 #endif
