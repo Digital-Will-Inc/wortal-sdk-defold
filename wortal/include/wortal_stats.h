e772a619f8c8af73f39d9c33a7ad176445b5caa2
Tim Dillon <tim@tim.jp.net>
2024-05-17 10:43:24 +0900

    feat(core): add stats api (#61)

---
+++ wortal/include/wortal_stats.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <dmsdk/sdk.h>
+
+#if defined(DM_PLATFORM_HTML5)
+
+class WortalStats {
+public:
+    typedef void (*OnGetStatsCallback)(const char* stats, const char* error);
+    typedef void (*OnPostStatCallback)(const int success, const char* error);
+
+    static int GetStatsAsync(lua_State* L);
+    static int PostStatsAsync(lua_State* L);
+
+private:
+    static void OnGetStats(const char* stats, const char* error);
+    static void OnPostStats(const int success, const char* error);
+};
+
+extern "C" {
+    void Wortal_stats_getStatsAsync(const char* level, const char* payload, WortalStats::OnGetStatsCallback callback);
+    void Wortal_stats_postStatsAsync(const char* level, const int value, const char* payload, WortalStats::OnPostStatsCallback callback);
+}
+
+#endif
