b1c31bc152ef48f9f899ae5d8a1f52283ebd1769
bimaswara <bimaswara@deepdivestudios.gg>
2024-11-08 13:27:59 +0700

    feat (core) : onResume

--- wortal/include/wortal.h
+++ wortal/include/wortal.h
@@ -4,36 +4,42 @@
 
 #if defined(DM_PLATFORM_HTML5)
 
-class Wortal {
+class Wortal
+{
 public:
     typedef void (*OnPauseCallback)(const int success);
-    typedef void (*OnHapticFeedbackCallback)(const int success, const char* error);
-    typedef void (*OnInitializeCallback)(const int success, const char* error);
-    typedef void (*OnStartGameCallback)(const int success, const char* error);
+    typedef void (*OnResumeCallback)(const int success);
+    typedef void (*OnHapticFeedbackCallback)(const int success, const char *error);
+    typedef void (*OnInitializeCallback)(const int success, const char *error);
+    typedef void (*OnStartGameCallback)(const int success, const char *error);
 
-    static int IsInitialized(lua_State* L);
-    static int InitializeAsync(lua_State* L);
-    static int StartGameAsync(lua_State* L);
-    static int SetLoadingProgress(lua_State* L);
-    static int SetPauseCallback(lua_State* L);
-    static int PerformHapticFeedback(lua_State* L);
-    static int GetSupportedAPIs(lua_State* L);
+    static int IsInitialized(lua_State *L);
+    static int InitializeAsync(lua_State *L);
+    static int StartGameAsync(lua_State *L);
+    static int SetLoadingProgress(lua_State *L);
+    static int SetPauseCallback(lua_State *L);
+    static int SetResumeCallback(lua_State *L);
+    static int PerformHapticFeedback(lua_State *L);
+    static int GetSupportedAPIs(lua_State *L);
 
 private:
-    static void OnInitialize(const int success, const char* error);
-    static void OnStartGame(const int success, const char* error);
+    static void OnInitialize(const int success, const char *error);
+    static void OnStartGame(const int success, const char *error);
     static void OnPause(const int success);
-    static void OnHapticFeedback(const int success, const char* error);
+    static void OnResume(const int success);
+    static void OnHapticFeedback(const int success, const char *error);
 };
 
-extern "C" {
+extern "C"
+{
     int Wortal_isInitialized();
     void Wortal_initializeAsync(Wortal::OnInitializeCallback callback);
     void Wortal_startGameAsync(Wortal::OnStartGameCallback callback);
     void Wortal_setLoadingProgress(const int progress);
     void Wortal_onPause(Wortal::OnPauseCallback callback);
+    void Wortal_onResume(Wortal::OnResumeCallback callback);
     void Wortal_performHapticFeedback(Wortal::OnHapticFeedbackCallback callback);
-    char* Wortal_getSupportedAPIs();
+    char *Wortal_getSupportedAPIs();
 }
 
 #endif
